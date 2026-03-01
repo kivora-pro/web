1366cee04a8a84ff3d30727c3051f2415165e068
JSCode <105148411+jscode-es@users.noreply.github.com>
2026-03-01 17:59:29 +0100

    feat: add page-level components

---
+++ src/components/pages/PageHome.tsx
@@ -0,0 +1,11 @@
+import { Background } from '@/components/Background';
+import { CarouselHome } from '@/components/carousel/CarouselHome';
+
+export function PageHome() {
+	return (
+		<>
+			<Background />
+			<CarouselHome />
+		</>
+	);
+}
